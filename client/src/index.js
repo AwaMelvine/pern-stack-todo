3a99b952f76eca0012bfaf4f0cfa9755894197ce
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-04-18 00:17:44 +0100

    Remove redux-devtools-extension and other dev dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
-import { BrowserRouter } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import App from './App';
-import './index.css';
-import rootReducer from './rootReducer';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+// import logger from "redux-logger";
+import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import App from "./App";
+import "./index.css";
+import rootReducer from "./rootReducer";
 
 const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-       applyMiddleware(thunk, logger)
-    )
-)
+  rootReducer
+  // composeWithDevTools(
+  //    applyMiddleware(thunk, logger)
+  // )
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>,
-document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
